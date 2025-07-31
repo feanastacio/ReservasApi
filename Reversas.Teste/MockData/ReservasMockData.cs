2f9b18d98af6aceecb07fb99caf63f8461f48076
Fernanda Marques <fernada.anastacio@aluno.senai.br>
2025-07-31 13:35:29 -0300

    Teste de software

---
+++ Reversas.Teste/MockData/ReservasMockData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reservas.Api.Models;
+
+namespace Reversas.Teste.MockData
+{
+    public class ReservasMockData
+    {
+        public static List<Reserva> GetReservas()
+        {
+            return new List<Reserva>() {
+                new Reserva {ReservaId = 1, Nome="Marcos", InicioLocacao="Sâo Paulo", FimLocacao="Campinas"},
+                new Reserva {ReservaId = 2, Nome="Fernanda", InicioLocacao="Sâo Paulo", FimLocacao="Rio de Janeiro"},
+                new Reserva {ReservaId = 3, Nome="Samantha", InicioLocacao="Sâo Paulo", FimLocacao="Niterói"}
+            };
+        }
+    }
+}
